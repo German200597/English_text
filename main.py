2dc83d4f40b7410bab5a4eeba86a030d8d51fa68
German200597 <89629715901LG@gmail.com>
2021-01-03 20:34:05 +0000

    no unnecessary lines

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-
-# for i in docs:
-# with open('1.txt', 'w') as final:
-#   with open('1.txt') as one:
-#     piece_one = one.readlines()
-#     size_one = len(piece_one)
-#   with open('2.txt') as two:
-#     piece_two = two.readlines()
-#     size_two = len(piece_two)
-#   with open('3.txt') as three:
-#     piece_three = three.readlines()
-#     size_three = len(piece_three)
-
-
-
 import os
 folder = os.listdir(path='/home/runner/Englishtext/docs')
 with open('4.txt', 'a') as final:
@@ -22,18 +7,6 @@ with open('4.txt', 'a') as final:
       piece = file.read()
     final.write(str(len(piece))+'\n')
     final.write(piece+'\n')
-    
-      
-    # with open (i) as text:
-    #   smile = text.readlines()
-    # final.write(smile)
-  #   with open(i) as text:
-  #     piece = text.readlines()
-  #     size_one = len(piece)
-  #     print(size_one)
-  #     final.write(os.path.basename(i))
-  #     print(size_one)
-  # sortirovka = sorted()   
    
 
 
