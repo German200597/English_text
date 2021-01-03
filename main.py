f235b389ac4e2a83a4910424ba6cff9f914599d2
German200597 <89629715901LG@gmail.com>
2021-01-03 22:17:25 +0000

    new version

--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import os
 folder = os.listdir(path='/home/runner/Englishtext/docs')
 folder.sort()
 with open('4.txt', 'a') as final:
-  folder.sort(key=len)
+  folder.sort()
   for i in folder:
+    counter=0
     with open(os.path.join('docs', i)) as file:
+      for line in i:
+        counter+=1
       final.write(os.path.basename(i)+'\n')
       piece = file.read()
-    final.write(str(len(piece))+'\n')
+    final.write(str(counter)+'\n')
     final.write(piece+'\n')
 
    
