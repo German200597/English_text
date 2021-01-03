31c3d034ef4f3f928cfeadc08777dd5bd1b6f73d
German200597 <89629715901LG@gmail.com>
2021-01-03 20:48:18 +0000

    no unnecessary lines

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 folder = os.listdir(path='/home/runner/Englishtext/docs')
+folder.sort()
 with open('4.txt', 'a') as final:
+  folder.sort(key=len)
   for i in folder:
     with open(os.path.join('docs', i)) as file:
       final.write(os.path.basename(i)+'\n')
       piece = file.read()
     final.write(str(len(piece))+'\n')
-    final.write(piece+'\n')
+    final.write(piece+'\n') 
    
 
 
