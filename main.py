5ee891540d5eba2f62cc2c1576dfdfe0a0b65120
German200597 <89629715901LG@gmail.com>
2021-01-04 19:36:38 +0000

    final version

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import os
-folder = os.listdir(path='/home/runner/Englishtext/docs')
+folder = os.listdir(path='/home/runner/Englishtext-1/docs')
 folder.sort()
+files = []
 with open('4.txt', 'a') as final:
-  folder.sort()
+  
   for i in folder:
     with open(os.path.join('docs', i)) as file:
-      counter=0
-      for line in i:
-        counter+=1
-      final.write(os.path.basename(i)+'\n')
       piece = file.read()
-    final.write(str(counter)+'\n')
-    final.write(piece+'\n')
+      files.append(
+        {
+          'name': i,
+          'content': piece,
+          'length': piece.count('\n')+1
+        } 
+      )
+  # Сортируем
+  files.sort(key=lambda x: x['length'])
+  # Печатаем
+  for entry in files:
+    final.write(entry['name'] + '\n')
+    final.write(str(entry['length'])+ '\n')
+    final.write(entry['content'] + '\n')
+    
+  # print(files)
 
    
 
